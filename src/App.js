07fd39641d32959c32eefb83e3371b38d9b447c9
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-10 12:54:58 -0700

    Test API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {  useEffect, useState } from 'react';
 
 function App() {
+
+const [queen, getQueen] = useState({})
+
+const getQueenInfo = () => {
+  return fetch('http://www.nokeynoshade.party/api/queens/89')
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+useEffect(() => {
+  getQueenInfo()
+})
+
   return (
     <div className="App">
       <header className="App-header">
