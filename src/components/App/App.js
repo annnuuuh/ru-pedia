de351f37be781e146002b75aec81147b5219c3fb
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-12 09:49:34 -0700

    Import queens component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
     <Nav />
+    <Main />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import {  useEffect, useState } from 'react';
+import Queens from '../Queens/Queens';
 
 const Main = () => {
-  
-  return (
 
+  return (
+    <Queens />
   )
 }
 
