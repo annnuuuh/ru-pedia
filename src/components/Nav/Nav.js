1b4a3ad44d3c682cfb68b99181ed4423b01e1c39
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-12 09:06:56 -0700

    Create Search component and import to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
-import {  useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import Search from '../Search/Search';
 
 const Nav = () => {
   return (
     <div className="nav">
       <button>Seasons</button>
       <button>Queens</button>
-      <h1>Re-Pedia</h1>
+      <h1>Ru-Pedia</h1>
       <Search />
     </div>
   )
---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+const Search = () => {
+  return (
+    <form className="search">
+    <input
+    type="text"
+    placeholder="She done already done had herses..."
+    />
+    </form>
+  )
+}
+
+export default Search;
