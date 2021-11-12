2ca054f1987c4b476e62ea86150540ed8f50e58a
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-12 15:46:48 -0700

    Add all seasons dropdown button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createRef } from 'react';
 import Search from '../Search/Search';
 import { Link } from 'react-router-dom';
+import './Nav.css'
 
 const Nav = () => {
+
+  const toggleDropdown = () => {
+    return (document.getElementById("myDropdown").classList.toggle("show"))
+  }
+
   return (
     <div className="nav">
-      <button>Seasons</button>
-      <button>Queens</button>
+      <div className="dropdown-container">
+        <button className="szns-btn"  onClick={toggleDropdown}>Seasons</button>
+        <div class="dropdown" id="myDropdown">
+            <Link to="/season1"><p>Season 1</p></Link>
+            <Link to="/season1"><p>Season 2</p></Link>
+            <Link to="/season1"><p>Season 3</p></Link>
+            <Link to="/season1"><p>Season 4</p></Link>
+            <Link to="/season1"><p>Season 5</p></Link>
+            <Link to="/season1"><p>Season 6</p></Link>
+            <Link to="/season1"><p>Season 7</p></Link>
+            <Link to="/season1"><p>Season 8</p></Link>
+            <Link to="/season1"><p>Season 9</p></Link>
+            <Link to="/season1"><p>Season 10</p></Link>
+            <Link to="/season1"><p>Season 11</p></Link>
+            <Link to="/season1"><p>Season 12</p></Link>
+        </div>
+      </div>
       <h1>Ru-Pedia</h1>
       <Search />
     </div>
