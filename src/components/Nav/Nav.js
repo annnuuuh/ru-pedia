8db972da7edab4084db16c015b1305364c86a1d1
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-12 08:42:21 -0700

    Create Nav Component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import {  useEffect, useState } from 'react';
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <button>Seasons</button>
+      <button>Queens</button>
+      <h1>Re-Pedia</h1>
+      <Search />
+    </nav>
+  )
+}
+
+export default Nav;
