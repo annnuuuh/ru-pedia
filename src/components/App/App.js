a89f9f8e30fe73f57ae954efc489e4a4ad54affd
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-12 09:14:18 -0700

    Create main component and import to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,9 @@
 import './App.css';
-import Nav from '../Nav/Nav'
+import Nav from '../Nav/Nav';
+import Main from '../Main/Main';
 import {  useEffect, useState } from 'react';
 
 function App() {
-
-const [queen, getQueen] = useState({})
-
-const getQueenInfo = () => {
-  return fetch('http://www.nokeynoshade.party/api/queens/89')
-  .then(response => response.json())
-  .then(data => console.log(data))
-}
-
-useEffect(() => {
-  getQueenInfo()
-})
 
   return (
     <div className="App">
