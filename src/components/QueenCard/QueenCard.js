475cb2a1b123e933796acf994e85befae0de52ba
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-13 14:07:22 -0700

    Finish styling queen images

--- src/components/QueenCard/QueenCard.js
+++ src/components/QueenCard/QueenCard.js
@@ -4,9 +4,9 @@ import './QueenCard.css';
 const QueenCard = ({ id, name, winner, missCon, img, quote, firstSeason }) => {
   return (
     <div className="queen-card">
-      <img src={img} className="queen-pic" alt="Queen Profile"/>
+      <img id={`id${id}`} src={img} className="queen-pic" alt="Queen Profile"/>
       <div className="middle">
-        <div className="text">"{quote}"></div>
+        <div className="text">"{quote}"</div>
       </div>
       <h3>{name}</h3>
       <p>Season {firstSeason}</p>
