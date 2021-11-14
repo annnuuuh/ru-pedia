009591929c5edc036c8057ab435b06f4f8fe97e6
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-13 20:41:34 -0700

    Add link to queen card

--- src/components/QueenCard/QueenCard.js
+++ src/components/QueenCard/QueenCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './QueenCard.css';
 
 const QueenCard = ({ id, name, winner, missCon, img, quote, firstSeason }) => {
   return (
-    <div className="queen-card">
-      <img id={`id${id}`} src={img} className="queen-pic" alt="Queen Profile"/>
-      <div className="middle">
-        <div className="text">"{quote}"</div>
+    <Link to={`/queens/${id}`}>
+      <div className="queen-card">
+        <img id={`id${id}`} src={img} className="queen-pic" alt="Queen Profile"/>
+        <div className="middle">
+          <div className="text">"{quote}"</div>
+        </div>
+        <h3>{name}</h3>
+        <p>Season {firstSeason} Winner </p>
       </div>
-      <h3>{name}</h3>
-      <p>Season {firstSeason} Winner </p>
-    </div>
+    </Link>
   )
 }
 export default QueenCard
