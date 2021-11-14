19fadb2b0dfa86ecddc47da09b93fa9d0dabc991
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-14 12:00:00 -0700

    Display all queen details on details page

--- src/components/QueenDetails/QueenDetails.js
+++ src/components/QueenDetails/QueenDetails.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 
 const QueenDetails = ({ id }) => {
   const [queenDetails, setQueenDetails] = useState({})
+  const [queenFirstSeason, setQueenFirstSeason] = useState({})
+  const [queenSecondSeason, setQueenSecondSeason] = useState({})
 
   const getQueenDetails = () => {
     return fetch(`http://www.nokeynoshade.party/api/queens/${id}`)
@@ -16,9 +18,27 @@ const QueenDetails = ({ id }) => {
     .then(queen => setQueenDetails(queen))
   }, [])
 
+  useEffect(() => {
+    getQueenDetails(id)
+    .then(data => data.seasons[0])
+    .then(seasons => setQueenFirstSeason(seasons))
+  })
+
+  useEffect(() => {
+    getQueenDetails(id)
+    .then(data => data.seasons[1])
+    .then(seasons => setQueenSecondSeason(seasons))
+  })
+
+
   return (
     <div className="queen-details">
       <h3>{queenDetails.name}</h3>
+      <img src={queenDetails.image_url} alt={queenDetails.name}/>
+      <p>Notable Quote: {queenDetails.quote}</p>
+      <p>Season: {queenFirstSeason.seasonNumber} Place: {queenFirstSeason.place}</p>
+      <p>Season: {queenSecondSeason.seasonNumber} Place: {queenSecondSeason.place}</p>
+
     </div>
 
   )
