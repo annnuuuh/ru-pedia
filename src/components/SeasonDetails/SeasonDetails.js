5b2af69515f2e04622fecaffffac1e4b549c5e42
Anna Johnson <ae.johnson2931@gmail.com>
2021-11-14 14:44:49 -0700

    Display season queens on season page

--- src/components/SeasonDetails/SeasonDetails.js
+++ src/components/SeasonDetails/SeasonDetails.js
@@ -4,8 +4,14 @@ import { useEffect, useState } from 'react';
 
 const SeasonDetails = ({ id }) => {
   const [seasonDetails, setSeasonDetails] = useState({})
+  const [seasonQueens, setSeasonQueens] = useState([])
 
   const getSeasonDetails = () => {
+    return fetch(`http://www.nokeynoshade.party/api/seasons/${id}`)
+    .then(response => response.json())
+  }
+
+  const getSeasonQueens = () => {
     return fetch(`http://www.nokeynoshade.party/api/seasons/${id}`)
     .then(response => response.json())
   }
@@ -16,11 +22,27 @@ const SeasonDetails = ({ id }) => {
     .then(season => setSeasonDetails(season))
   }, [id])
 
+  useEffect(() => {
+    getSeasonQueens(id)
+    .then(data => data)
+    .then(queen => setSeasonQueens(queen.queens))
+  }, [id])
+
+  const seasonQueenCards = seasonQueens.map(queen => {
+    return (
+      <div className="season-queen-card">
+        <p>{queen.name}</p>
+        <p>Place: {queen.place}</p>
+      </div>
+    )
+  })
+
   return (
     <div className="season-details">
       <h3>{seasonDetails.seasonNumber}</h3>
       <img src={seasonDetails.image_url} alt={seasonDetails.name}/>
-
+      <p>Contestants:</p>
+      <div>{seasonQueenCards}</div>
     </div>
 
   )
